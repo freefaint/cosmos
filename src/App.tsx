74e6f6fc9a3bab65b8e91a6a3c9acb08496287e7
Андрей Терещенко <andrejteresenko@MacBook-Air-Andrej.local>
2024-04-26 16:57:21 +0300

    fixed something

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ const Screen = styled.div`
   overscroll-behavior: none;
 `;
 
-const scale = 2.6294577802732605e-9;
-const timeScale = 60 * 60 * 24 * 30;
+const scale = 16.6294577802732605e-8;
+const timeScale = 10000; // * 60 * 60 * 24 * 100;
 
-const fps = 100;
+const fps = 60;
 
 const G = 6.67430 * Math.pow(10, -11)
 
@@ -51,12 +51,33 @@ const objects: Planet[] = [
 
     speed: {
       x: 0,
-      y: 0,
+      y: -126778,
       z: 0
     },
 
     position: {
       x: 0,
+      y: 0,
+      z: 0
+    }
+  },
+  {
+    name: 'Sun2',
+    color: '#fca',
+
+    params: {
+      radius: 695500000,
+      mass: 1.989e30,
+    },
+
+    speed: {
+      x: 0,
+      y: 126778,
+      z: 0
+    },
+
+    position: {
+      x: 3695500000,
       y: 0,
       z: 0
     }
@@ -113,7 +134,7 @@ const objects: Planet[] = [
     },
 
     speed: {
-      x: 30000,
+      x: 29780,
       y: 0,
       z: 0
     },
@@ -144,11 +165,77 @@ const objects: Planet[] = [
       y: -1020, // m per sec
       z: 0
     }
-  }
+  },
+  {
+    name: 'Mars',
+    color: '#f55',
+
+    params: {
+      radius: 3389500,
+      mass: 6.41693e23,
+    },
+
+    speed: {
+      x: 26500,
+      y: 0,
+      z: 0
+    },
+
+    position: {
+      x: 0,
+      y: 225000000000,
+      z: 0
+    }
+  },
+
+  // ...(new Array(200).fill(true).map(i => ({
+  //   name: i + 'i',
+  //   color: '#fff',
+
+  //   params: {
+  //     radius: Math.random() * 10000000,
+  //     mass: Math.random() * 6.41693e24,
+  //   },
+
+  //   speed: {
+  //     x: Math.random() * 30000,
+  //     y: 0,
+  //     z: 0
+  //   },
+
+  //   position: {
+  //     x: 0,
+  //     y: 225000000000 - Math.random() * 100000000000,
+  //     z: 0
+  //   }
+  // }))),
+
+  // ...(new Array(200).fill(true).map(i => ({
+  //   name: i + 'ii',
+  //   color: '#fff',
+
+  //   params: {
+  //     radius: Math.random() * 10000000,
+  //     mass: Math.random() * 6.41693e24,
+  //   },
+
+  //   speed: {
+  //     x: - Math.random() * 30000,
+  //     y: 0,
+  //     z: 0
+  //   },
+
+  //   position: {
+  //     x: 0,
+  //     y: - 225000000000 + Math.random() * 100000000000,
+  //     z: 0
+  //   }
+  // })))
 ];
 
 class PlanetObject {
-  public node: HTMLDivElement;
+  readonly node: HTMLDivElement;
+  public hidden: boolean = false;
 
   constructor(readonly name: string, readonly color: string, readonly params: Attributes, readonly position: Position, readonly speed: Speed) {
     this.node = document.createElement('div');
@@ -158,9 +245,9 @@ class PlanetObject {
   }
 
   public draw = (scale: number, offset: Coords) => {
-    const width = this.params.radius;
-    const height = this.params.radius;
-    const depth = this.params.radius;
+    const width = this.params.radius * 2;
+    const height = this.params.radius * 2;
+    const depth = this.params.radius * 2;
 
     const x = offset.x + this.position.x - width / 2;
     const y = offset.y + this.position.y - height / 2;
@@ -184,6 +271,32 @@ class PlanetObject {
     this.position.x += this.speed.x * scale;
     this.position.y += this.speed.y * scale;
   }
+
+  public show = () => {
+    this.hidden = false;
+    this.node.style.display = 'block';
+  }
+
+  public hide = () => {
+    this.hidden = true;
+    this.node.style.display = 'none';
+  }
+}
+
+const sum = (a: Coords, b: Coords) => {
+  return { x: a.x + b.x, y: a.y + b.y, z: a.z + b.z };
+}
+
+const roots = (A: number, B: number, C: number) => {
+  const D = Math.pow(B, 2) - 4 * A * C;
+
+  if (!D) {
+    return [- B / (2 * A)];
+  } else if (D < 0) {
+    return;
+  } else {
+    return [(- B + Math.sqrt(D)) / (2 * A), (- B - Math.sqrt(D)) / (2 * A)]
+  }
 }
 
 function App() {
@@ -191,9 +304,10 @@ function App() {
   const date = useRef<number>(0);
   const ref = useRef<HTMLDivElement>(null);
   const dateRef = useRef<HTMLDivElement>(null);
+  const cameraRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (!ref.current) {
+    if (!ref.current || !cameraRef.current) {
       return;
     }
 
@@ -209,19 +323,19 @@ function App() {
     canvas.width = 2000;
     canvas.height = 2000;
 
-    for (var i = 0; i < 20; i ++) {
+    for (var i = 0; i < 100; i ++) {
       ctx.strokeStyle = `rgba(255, 255, 255, ${i % 10 ? 0.2 : 0.5})`;
       ctx.beginPath();
-      ctx.moveTo(0, i * 100);
-      ctx.lineTo(2000, i * 100);
-      ctx.lineWidth = 1;
+      ctx.moveTo(0, i * 20);
+      ctx.lineTo(2000, i * 20);
+      ctx.lineWidth = 0.5;
       ctx.stroke();
 
       ctx.strokeStyle = `rgba(255, 255, 255, ${i % 10 ? 0.2 : 0.5})`;
       ctx.beginPath();
-      ctx.moveTo(i * 100, 0);
-      ctx.lineTo(i * 100, 2000);
-      ctx.lineWidth = 1;
+      ctx.moveTo(i * 20, 0);
+      ctx.lineTo(i * 20, 2000);
+      ctx.lineWidth = 0.5;
       ctx.stroke();
     }
 
@@ -281,6 +395,34 @@ function App() {
 
     const startDate = new Date().valueOf();
 
+
+
+    cameraRef.current.width = 320;
+    cameraRef.current.height = 240;
+
+    const context = cameraRef.current.getContext("2d");
+    const width = cameraRef.current.width;
+    const height = cameraRef.current.height;
+
+    let rays: Coords[] = [];
+
+    const xAngle = 40; // 45;
+    const yAngle = 30; // 30;
+
+    const cameraPos = {
+      x: 0,
+      y: 0,
+      z: 10_000_000_000
+    };
+
+    const cameraDirection = {
+      x: 0,
+      y: Math.sqrt(2) / 2,
+      z: Math.sqrt(2) / 2
+    }
+
+    
+
     const interval = setInterval(() => {
       date.current += 1000 * timeScale / fps;
       dateRef.current!.innerHTML = new Date(startDate + date.current).toLocaleString()
@@ -288,7 +430,9 @@ function App() {
 
       // scale bg
       const zeroScale = Math.pow(10, Math.floor(Math.log10(currentScale)));
-      ref.current!.style.backgroundSize = currentScale / zeroScale * 35 + '%';
+      ref.current!.style.backgroundSize = (currentScale / zeroScale > 5 ? 30 * currentScale / zeroScale : 60 * currentScale / zeroScale) + '%';
+
+      // console.log(currentScale / zeroScale > 5);
 
       nodes.forEach(i => {
         nodes.forEach(j => {
@@ -317,15 +461,105 @@ function App() {
         });
       });
 
+      // console.log(1 / currentScale);
+
       nodes.forEach(i => {
         i.affect(timeScale / fps);
+
+        // const left = (offset.x - ref.current!.clientWidth / 2) / currentScale;
+        // const top = (offset.y - ref.current!.clientHeight / 2) / currentScale;
+        // const right = (offset.x + ref.current!.clientWidth / 2) / currentScale;
+        // const bottom = (offset.y + ref.current!.clientHeight / 2) / currentScale;
+
         i.draw(currentScale, { x: ref.current!.clientWidth / 2 + offset.x, y: ref.current!.clientHeight / 2 + offset.y, z: 0 });
+
+        // const inScreen = i.position.x > left && i.position.x < right && i.position.x > top && i.position.y < bottom;
+        // const notFar = 1 / currentScale < i.params.radius * 1000;
+        // console.log(i.params);
+
+        const needDraw = true; // inScreen; // && notFar;
+
+        if (needDraw) {
+          if (i.hidden) {
+            i.show();
+          }
+        } else {
+          if (!i.hidden) {
+            i.hide();
+          }
+        }
+
         // console.log(flow);
         if (flow.current === i.name) {
           offset.x = - i.position.x * currentScale;
           offset.y = - i.position.y * currentScale;
         }
       });
+
+      
+
+      // 3d engine
+
+      let pixels: boolean[] = [];
+
+      for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+          const angleFromCenterX = - xAngle * (x - width / 2) / width;
+          const angleFromCenterY = - yAngle * (y - height / 2) / height;
+
+          // const xLen = Math.sin(angleFromCenterX * Math.PI / 180);
+          // const yLen = Math.sin(angleFromCenterY * Math.PI / 180);
+          // const zLen = Math.sqrt(1 - Math.pow(xLen, 2) - Math.pow(yLen, 2));
+
+
+          const X = Math.tan(angleFromCenterX * Math.PI / 180);
+          const Y = Math.tan(angleFromCenterY * Math.PI / 180);
+          const Z = - 1;
+
+          // console.log(X, Y, Z);
+
+          const sum = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2) + Math.pow(Z, 2));
+
+          const xDir = X / sum;
+          const yDir = Y / sum;
+          const zDir = Z / sum;
+
+          const xLen = Math.cos(Math.acos(xDir));
+          const yLen = Math.cos(Math.acos(yDir));
+          const zLen = Math.cos(Math.acos(zDir));
+
+          // const angleVector: Coords = { x: xLen, y: yLen, z: zLen };
+
+          let isPainted = false;
+
+          nodes.forEach(i => {
+            const A = 1; // Math.pow(xLen, 2) + Math.pow(yLen, 2) + Math.pow(zLen, 2);
+            const B = 2 * (cameraPos.x - i.position.x) * xLen + 2 * (cameraPos.y - i.position.y) * yLen + 2 * (cameraPos.z - i.position.z) * zLen;
+            const C = Math.pow(cameraPos.x - i.position.x, 2) + Math.pow(cameraPos.y - i.position.y, 2) + Math.pow(cameraPos.z - i.position.z, 2) - Math.pow(i.params.radius, 2);
+
+            if (!isPainted) {
+              isPainted = !!roots(A, B, C);
+            }
+          });
+
+          pixels.push(isPainted)
+        }
+      }
+
+
+
+      // draw 3d on canvas engine
+
+      var imageData = context!.getImageData(0,0, width, height);
+
+      for (var i = 0; i < imageData.data.length; i += 4) {
+        imageData.data[i] = pixels[i / 4] ? 255 : 0;  // установка серого цвета
+        imageData.data[i + 1] = pixels[i / 4] ? 255 : 0;
+        imageData.data[i + 2] = pixels[i / 4] ? 255 : 0;
+        imageData.data[i + 3] = 255;
+      }
+        
+      context!.putImageData(imageData, 0, 0);
     }, 1000 / fps);
 
     return () => {
@@ -335,24 +569,24 @@ function App() {
     }
   }, []);
 
-  useEffect(() => {
-
-  }, [dateRef]);
-
   return (
-    <Screen ref={ref}>
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <select value={flow.current} onChange={e => flow.current = e.target.value}>
-          <option />
+    <>
+      <Screen ref={ref}>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <select value={flow.current} onChange={e => flow.current = e.target.value}>
+            <option />
 
-          {objects.map(i => (
-            <option value={i.name}>{i.name}</option>
-          ))}
-        </select>
+            {objects.map(i => (
+              <option value={i.name}>{i.name}</option>
+            ))}
+          </select>
 
-        <span style={{ color: "#fff", fontSize: "12px", fontFamily: "monospace" }} ref={dateRef} />
-      </div>
-    </Screen>
+          <span style={{ color: "#fff", fontSize: "12px", fontFamily: "monospace" }} ref={dateRef} />
+        </div>
+      </Screen>
+
+      <canvas style={{ filter: "blur(1px)", opacity: 0.8, position: "fixed", bottom: "16px", right: "16px", border: "1px solid #ccc" }} ref={cameraRef} />
+    </>
   );
 }
 
